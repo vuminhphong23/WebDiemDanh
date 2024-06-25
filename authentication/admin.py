1e799a4a625b25ccd2c0a69070a31698eb6c978a
vuminhphong23 <phongvuminh2003@gmail.com>
2024-06-25 21:04:25 +0700

    edit

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,70 @@
 from django.contrib import admin
+from .models import TblStudents, Classroom, AttendanceSession, Attendance
 
+class TblStudentsInline(admin.TabularInline):
+    model = TblStudents.classrooms.through
+    extra = 1
 
+class AttendanceInline(admin.TabularInline):
+    model = Attendance
+    extra = 1
+    raw_id_fields = ('session', 'student')
 
+@admin.register(TblStudents)
+class TblStudentsAdmin(admin.ModelAdmin):
+    list_display = ('student_id', 'name', 'email', 'phone', 'date_birth', 'iCap', 'display_classrooms')
+    search_fields = ('name', 'email', 'phone')
+    list_filter = ('date_birth', 'classrooms', 'iCap')
+    exclude = ('password',)
 
+    def display_classrooms(self, obj):
+        return ", ".join([classroom.class_name for classroom in obj.classrooms.all()])
+
+    display_classrooms.short_description = 'Classrooms'
+    readonly_fields = ('student_id',)
+    filter_horizontal = ('classrooms',)
+
+    def save_model(self, request, obj, form, change):
+        if form.is_valid():
+            raw_password = form.cleaned_data.get('password')
+            if raw_password and not obj.pk:
+                obj.set_password(raw_password)
+        super().save_model(request, obj, form, change)
+
+@admin.register(Classroom)
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ('class_id', 'class_name', 'teacher_name', 'display_students')
+    search_fields = ('class_name', 'teacher_name')
+    inlines = [TblStudentsInline]
+
+    def display_students(self, obj):
+        return ", ".join([student.name for student in obj.students.all()])
+
+    display_students.short_description = 'Students'
+
+class AttendanceSessionInline(admin.TabularInline):
+    model = AttendanceSession
+    extra = 1
+    show_change_link = True
+
+@admin.register(AttendanceSession)
+class AttendanceSessionAdmin(admin.ModelAdmin):
+    list_display = ('session_id', 'classroom', 'date', 'start_time', 'end_time')
+    search_fields = ('classroom__class_name', 'date')
+    list_filter = ('classroom', 'date')
+    inlines = [AttendanceInline]
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('session', 'student', 'attended', 'datetime')
+    search_fields = ('session__classroom__class_name', 'student__name', 'session__date')
+    list_filter = ('session__classroom', 'session__date', 'attended')
+    raw_id_fields = ('session', 'student')
+    fields = ('session', 'student', 'datetime', 'attended')
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'student':
+            session_id = request.GET.get('session')
+            if session_id:
+                kwargs['queryset'] = db_field.related_model.objects.filter(classrooms__attendance_sessions__id=session_id).distinct()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
--- authentication/models.py
+++ authentication/models.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
 class Classroom(models.Model):
     class_id = models.AutoField(primary_key=True)
@@ -11,18 +13,44 @@ class Classroom(models.Model):
 class TblStudents(models.Model):
     student_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
     phone = models.CharField(max_length=15)
+    iCap = models.BooleanField(default=False)
     date_birth = models.DateField()
-    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, related_name='students', null=True)
+    password = models.CharField(max_length=128, default=make_password('abc@123'))
+    classrooms = models.ManyToManyField(Classroom, related_name='students')
 
     def __str__(self):
         return self.name
-    
-class Attendance(models.Model):
-    student = models.ForeignKey(TblStudents, on_delete=models.CASCADE)
-    datetime = models.DateTimeField(auto_now_add=True) 
-    attended = models.BooleanField(default=False)
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+class AttendanceSession(models.Model):
+    session_id = models.AutoField(primary_key=True)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, related_name='attendance_sessions')
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
 
     def __str__(self):
-        return f"{self.student.name} - {self.datetime} - {'Present' if self.attended else 'Absent'}"
+        return f"{self.classroom.class_name} - {self.date} ({self.start_time} - {self.end_time})"
+
+class Attendance(models.Model):
+    session = models.ForeignKey(AttendanceSession, on_delete=models.CASCADE, related_name='attendances',null=True)
+    student = models.ForeignKey(TblStudents, on_delete=models.CASCADE, related_name='attendances')
+    attended = models.BooleanField(default=False)
+    datetime = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.student.name} - {self.session.classroom.class_name} - {self.session.date}"
+
+    def save(self, *args, **kwargs):
+        if not self.datetime:
+            self.datetime = datetime .now()
+        if not (self.session.start_time <= self.datetime.time() <= self.session.end_time):
+            self.attended = False
+        super().save(*args, **kwargs)
