a604aa3c1809fedb7d8f3b41ae5c55999bd3f1ad
vuminhphong23 <phongvuminh2003@gmail.com>
2024-06-28 15:12:43 +0700

    css

--- authentication/classStudentController.py
+++ authentication/classStudentController.py
@@ -1,5 +1,5 @@
 # classStudentController.py
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Classroom, TblStudents
 
 def view(request):
@@ -9,13 +9,17 @@ def view(request):
         classrooms = student.classrooms.all()  # Fetch all classrooms the student is registered in
         return render(request, "student/class_students.html", {'classrooms': classrooms})
     else:
-        return render(request, "student/class_students.html", {'classrooms': []})
+        return redirect('home')
 
 def viewdetail(request, class_id):
-    classroom = get_object_or_404(Classroom, pk=class_id)
-    students = classroom.students.all()
-    context = {
-        'classroom': classroom,
-        'students': students
-    }
-    return render(request, 'student/class_detail.html', context)
+    student_id = request.session.get('student_id')  # Assuming the student ID is stored in the session
+    if student_id:
+        classroom = get_object_or_404(Classroom, pk=class_id)
+        students = classroom.students.all()
+        context = {
+            'classroom': classroom,
+            'students': students
+        }
+        return render(request, 'student/class_detail.html', context)
+    else:
+        return redirect('home')
